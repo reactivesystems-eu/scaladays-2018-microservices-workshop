5bb46d658d3f25c6276c1fb4970312edeafc0b08
Lutz Huehnken <lutz.huehnken@reactivesystems.eu>
2018-05-14 16:55:44 +0200

    Booking Service Descriptor

--- booking-api/src/main/scala/eu/reactivesystems/workshop/booking/api/BookingService.scala
+++ booking-api/src/main/scala/eu/reactivesystems/workshop/booking/api/BookingService.scala
@@ -1,6 +1,8 @@
 package eu.reactivesystems.workshop.booking.api
 
-import akka.NotUsed
+import java.util.UUID
+
+import akka.{Done, NotUsed}
 import com.lightbend.lagom.scaladsl.api.transport.Method
 import com.lightbend.lagom.scaladsl.api.{Service, ServiceCall}
 
@@ -11,11 +13,38 @@ trait BookingService extends Service {
 
   def healthCheck(): ServiceCall[NotUsed, String]
 
+  def cancelBooking(roomId: UUID, bookingId: UUID): ServiceCall[NotUsed, Done]
+
+  def confirmBooking(roomId: UUID, bookingId: UUID): ServiceCall[NotUsed, Done]
+
+  def requestBooking(roomId: UUID): ServiceCall[BookingRequest, UUID]
+
+  def rejectBooking(roomId: UUID, bookingId: UUID): ServiceCall[NotUsed, Done]
+
+  def withdrawBooking(roomId: UUID, bookingId: UUID): ServiceCall[NotUsed, Done]
+
+  def listRoom(roomId: UUID): ServiceCall[NotUsed, Done]
+
+  def unlistRoom(roomId: UUID): ServiceCall[NotUsed, Done]
+
   final override def descriptor = {
     import Service._
 
     named("booking")
-      .withCalls(restCall(Method.GET, "/api/booking/healthCheck", healthCheck))
+      .withCalls(restCall(Method.GET, "/api/room/healthCheck", healthCheck),
+        // confirm booking
+        restCall(Method.POST, "/api/room/:roomId/bookings/:bookingId/confirm", confirmBooking _),
+        // request booking
+        restCall(Method.POST, "/api/room/:roomId/bookings", requestBooking _),
+        // cancel booking
+        restCall(Method.DELETE, "/api/room/:roomId/bookings/:bookingId", cancelBooking _),
+        // reject booking
+        restCall(Method.POST, "/api/room/:roomId/bookings/:bookingId/reject", rejectBooking _),
+        // withdraw booking
+        restCall(Method.POST, "/api/bookings/:roomId/bookings/:bookingId/withdraw", withdrawBooking _),
+        restCall(Method.POST, "/api/room/:roomId", listRoom _),
+        restCall(Method.DELETE, "/api/room/:roomId", unlistRoom _))
+      // TODO later modify booking
       .withAutoAcl(true)
   }
 }
--- booking-impl/src/main/scala/eu/reactivesystems/workshop/booking/impl/BookingServiceImpl.scala
+++ booking-impl/src/main/scala/eu/reactivesystems/workshop/booking/impl/BookingServiceImpl.scala
@@ -2,21 +2,49 @@ package eu.reactivesystems.workshop.booking.impl
 
 import java.util.UUID
 
-import akka.NotUsed
+import akka.{Done, NotUsed}
 import com.lightbend.lagom.scaladsl.api.ServiceCall
 import com.lightbend.lagom.scaladsl.persistence.PersistentEntityRegistry
-import eu.reactivesystems.workshop.booking.api.BookingService
+import eu.reactivesystems.workshop.booking.api.{BookingRequest, BookingService}
 
 import scala.concurrent.{ExecutionContext, Future}
 
 class BookingServiceImpl(persistentEntityRegistry: PersistentEntityRegistry)(
-    implicit ec: ExecutionContext)
-    extends BookingService {
+  implicit ec: ExecutionContext)
+  extends BookingService {
 
   override def healthCheck(): ServiceCall[NotUsed, String] =
     request => Future.successful("OK")
 
-  private def entityRef(listingId: UUID) =
-    persistentEntityRegistry.refFor[BookingRegister](listingId.toString)
+
+  override def cancelBooking(roomId: UUID, bookingId: UUID): ServiceCall[NotUsed, Done] =
+    request => Future.successful(Done)
+
+  override def confirmBooking(roomId: UUID, bookingId: UUID): ServiceCall[NotUsed, Done] =
+    request => Future.successful(Done)
+
+
+  override def requestBooking(roomId: UUID): ServiceCall[BookingRequest, UUID] =
+    request => Future.successful(UUID.randomUUID())
+
+
+  override def rejectBooking(roomId: UUID, bookingId: UUID): ServiceCall[NotUsed, Done] =
+    request => Future.successful(Done)
+
+
+  override def withdrawBooking(roomId: UUID, bookingId: UUID): ServiceCall[NotUsed, Done] =
+    request => Future.successful(Done)
+
+
+  override def listRoom(roomId: UUID): ServiceCall[NotUsed, Done] =
+    request => Future.successful(Done)
+
+
+  override def unlistRoom(roomId: UUID): ServiceCall[NotUsed, Done] =
+    request => Future.successful(Done)
+
+
+  private def entityRef(roomId: UUID) =
+    persistentEntityRegistry.refFor[BookingRegister](roomId.toString)
 
 }
